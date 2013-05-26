d2085944dda8ca94fd34eaddfc8fcd2b8440b646
Woojin Joe <jwj081@gmail.com>
2013-05-26 23:47:02 +0900

    ver 0.999 KCP Module Chrome encoding fix, Tax Refund page Plus

---
+++ Page5-RefundInfo.php
@@ -0,0 +1,32 @@
+<?php 
+
+/**
+ * Template Name: Tax Refund Page
+ *
+ * A custom page template without sidebar.
+ * 
+ * @package WordPress
+ * @subpackage Hananuri
+ * @author NFM (jwj0831@gmail.com)
+ * @since Hananuri 1.0
+ */
+
+get_header(); ?>
+	<div class="subpage_stretched">
+		<div class="breadcrumb_stretched">
+			<div class="breadcrumb_fixed">
+				<p>
+					<img src='<?php bloginfo('template_url'); ?>/images/subpage/sub_home.gif'> 홈 > 후원 > 소득공제 정보
+				</p>
+			</div>
+		</div>
+		
+		<div class="subpage_fixed">
+			<?php include (TEMPLATEPATH . '/sidebar.php'); ?> 
+			<div id="content"><br />
+				<?php if (function_exists('iphorm')) echo iphorm(3); ?>
+			</div><!--close content-->
+		</div><!--close subpage_fixed-->
+	</div><!--close subpage_stretched-->
+	<div class="clear"></div>
+<?php get_footer(); ?>
