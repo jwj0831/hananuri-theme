813d0e52085a9311531d0a717c0322f9b41ac2c5
NFM <jwj0831@gmail.com>
2013-11-26 13:10:19 +0900

    update map link

--- donation_2.php
+++ donation_2.php
@@ -1,7 +1,7 @@
 <img src='/hiddenforsecurity/wordpress/wp-content/themes/hananuri_v1/images/single/support_single_top.gif'>
 <img id="knit_img" src='/hiddenforsecurity/wordpress/wp-content/themes/hananuri_v1/images/subpage/sub5/sub5_north_img.jpg' usemap="#North">
 <map name="North" id="Map">
-	<area shape="rect" coords="2,1932,220,2054" href="<?php echo home_url() ?>/support/temporal_support" />
-  	<area shape="rect" coords="220,1933,453,2053" href="<?php echo home_url() ?>/support/periodic_support" />
-  	<area shape="rect" coords="452,1933,710,2055" href="<?php echo home_url() ?>/support/money_box" />
+	<area shape="rect" coords="2,1932,220,2054" href="http://www.hananuri.org/support/temporal_support" />
+  	<area shape="rect" coords="220,1933,453,2053" href="http://www.hananuri.org/support/periodic_support" />
+  	<area shape="rect" coords="452,1933,710,2055" href="http://www.hananuri.org/support/money_box" />
 </map>
